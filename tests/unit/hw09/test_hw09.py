006ddbf46af2e0af7e7bff688ef020e9abcd41c1
Alb M <rinkorn.alb@gmail.com>
2023-10-11 16:24:05 +0300

    Update test_hw09

--- tests/unit/hw09/test_hw09.py
+++ tests/unit/hw09/test_hw09.py
@@ -1,11 +1,23 @@
-from spacegame.hw09.hw09 import InitScopeBasedIoCImplementationCmd, IoC
+import pytest
+
+
+def import_and_init_IoC():
+    from spacegame.hw09.hw09 import InitScopeBasedIoCImplementationCmd, IoC
+
+    InitScopeBasedIoCImplementationCmd().execute()
+    return IoC
 
 
 def test_root_scope_is_available():
-    InitScopeBasedIoCImplementationCmd().execute()
+    IoC = import_and_init_IoC()
     assert IoC.resolve("scopes.root") is not None
+    IoC.resolve("IoC.register", "a", lambda *args: 123).execute()
+    del IoC
 
 
 def test_create_scope_is_possible_at_any_moment():
-    InitScopeBasedIoCImplementationCmd().execute()
-    assert IoC.resolve("scopes.new", IoC.resolve("scopes.root")) is not None
+    IoC = import_and_init_IoC()
+    parent_scope = IoC.resolve("scopes.root")
+    assert IoC.resolve("scopes.new", parent_scope) is not None
+    assert IoC.resolve("a") == 123
+    del IoC
